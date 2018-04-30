abeb09b32d2e31806031eef156e0a0b4f8dc9cad
codyfitch <35245717+codyfitch@users.noreply.github.com>
2018-04-30 12:35:32 -0600

    Final Unity Project

---
+++ SpaceShooter/Assets/_Complete-Game/Scripts/PowerupCollide.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerupCollide : MonoBehaviour {
+
+    public GameObject PowerS;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Boundary")
+        {
+            return;
+        }
+
+        if (other.tag == "Player" || other.tag == "Shield")
+        {
+            //PowerS.SetActive(false);
+            Destroy(PowerS);
+        } 
+    }
+}
---
+++ SpaceShooter/Assets/_Complete-Game/Scripts/ShieldActive.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldActive : MonoBehaviour
+
+{
+    public GameObject shield;
+    
+
+    void OnTriggerEnter(Collider other)
+    {
+       /* if (other.tag == "Boundary")
+        {
+            return;
+        }*/
+
+        if(other.tag == "Powerup")
+        {
+            shield.gameObject.SetActive(true);
+        }
+
+        if(other.tag == "Enemy")
+        {
+            shield.gameObject.SetActive(false);
+        }
+    }
+}
